d48516d8d58b89515535db8c135f9d08c9ba6df9
DomVans <139610057+DomVans@users.noreply.github.com>
2025-10-06 17:12:58 +0530

    Add full project

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Text } from 'vue';
+
+</script>
+
+<template>
+    <div class="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500">
+    <h1 class="text-4xl font-bold text-white">🎸 Guitar Course App</h1>
+    <div>
+        <button class="ml-4 bg-white text-purple-500 font-bold py-2 px-4 rounded-full shadow-lg hover:bg-purple-100 transition duration-300 hover:opacity-80 cursor-pointer">
+            Sign Up
+        </button>
+        <button class="ml-4 bg-white text-purple-500 font-bold py-2 px-4 rounded-full shadow-lg hover:bg-purple-100 transition duration-300 hover:opacity-80 cursor-pointer">
+            Log In
+        </button>
+    </div>
+  </div>
+</template>
